69882b26707fa9b38383ddf908e6de70a7dfb69e
M4GNV5 <eragonjml@googlemail.com>
2016-12-13 15:05:38 +0100

    add dlist task

---
+++ dlist/DList.java
@@ -0,0 +1,25 @@
+package dlist;
+
+public class DList<T>
+{
+	DListEntry<T> head;
+
+	public void insert(int index, T value) //*** 1 ***
+	{
+		//*** 1 ***
+	}
+	
+	//*** 2 ***
+	
+	//*** 3 ***
+	
+	public T remove(int index) //*** 5 ***
+	{
+		//*** 5 ***
+	}
+	
+	public int length()
+	{
+		//*** 6 ***
+	}
+}
---
+++ dlist/DListEntry.java
@@ -0,0 +1,3 @@
+package dlist;
+
+//*** 0 ***
