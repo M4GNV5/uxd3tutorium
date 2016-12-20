a892d266ef08695648c2e18a60060a264c3ac02a
M4GNV5 <eragonjml@googlemail.com>
2016-12-20 15:40:26 +0100

    add binary tree (vs linked list) task

---
+++ tree/BinaryTree.java
@@ -0,0 +1,26 @@
+package tree;
+
+public class BinaryTree
+{
+	BinaryNode root;
+	
+	public BinaryTree(int start)
+	{
+		root = //*** 2 ***
+	}
+	
+	public void add(int val)
+	{
+		//*** 3 ***
+	}
+	
+	public boolean has(int val)
+	{
+		//*** 4 ***
+	}
+	
+	public void remove(int val)
+	{
+		//*** E1 ***
+	}
+}
