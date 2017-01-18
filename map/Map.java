7130f8f809bd1760dea88471cc2c3f9d7b619565
M4GNV5 <eragonjml@googlemail.com>
2017-01-18 08:58:17 +0100

    add map task

---
+++ map/Map.java
@@ -0,0 +1,23 @@
+package map;
+
+public class Map //*** 1 ***
+{
+	String key;
+	/* *** 1 *** */ value;
+	/* *** 1 *** */ next;
+	
+	public T get(String key)
+	{
+		//*** 2 ***
+	}
+	
+	public void set(String key, T value)
+	{
+		//*** 3 ***
+	}
+	
+	public boolean has(String key)
+	{
+		//*** 4 ***
+	}
+}
